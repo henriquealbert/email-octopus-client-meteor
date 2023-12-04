616f434ee03f13321c32ed2cd8203828dbfe3b51
Henrique A. Schmaiske <ishenriquealbert@gmail.com>
2023-12-04 16:40:50 -0300

    use lowest api version as possible

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: "hschmaiske:email-octopus",
-  version: "0.0.2",
+  version: "0.0.4",
   summary: "Email Octopus API Wrapper for Meteor",
   git: "https://github.com/henriquealbert/email-octopus-client-meteor",
   documentation: "README.md",
@@ -11,7 +11,7 @@ Npm.depends({
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom("2.13.3");
-  api.use("ecmascript");
+  api.versionsFrom("1.12.1");
+  api.use("ecmascript@0.13.0");
   api.mainModule("server-main.js", ["server"]);
 });
